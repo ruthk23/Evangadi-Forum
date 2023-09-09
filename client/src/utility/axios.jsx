658818ce9a9ad4cb1e2db43abafd3f4146961a95
ruthk23 <135168068+ruthk23@users.noreply.github.com>
2023-09-09 13:06:25 -0400

    evangadformreadydploy

---
+++ client/src/utility/axios.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const instance = axios.create({
+	baseURL: "http://localhost:4500", // api url
+	// baseURL: "https://evangadifinal.onrender.com/", // api url
+
+	withCredentials: true,
+});
+
+export default instance;
